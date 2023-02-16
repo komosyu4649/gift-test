e5d74b62f3fac2c42b4fe92255c95329d1a8c6d2
komosyu4649 <komosyu4649@gmail.com>
2023-02-16 10:25:16 +0900

    add: microcms data fetch

---
+++ src/lib/microcms/getData.ts
@@ -0,0 +1,8 @@
+import { client } from "."
+
+export const getMicroCMSData = async(contentId:string) => {
+    const res = await client.getList({
+        endpoint: contentId
+    })
+    return res
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,20 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import { GetStaticProps } from 'next'
+import { getMicroCMSData } from '@/lib/microcms/getData'
 
 const inter = Inter({ subsets: ['latin'] })
+
+export const getStaticProps: GetStaticProps = async() => {
+  const microcmsData = await getMicroCMSData("articles")
+  const microcmsDataContents = microcmsData.contents
+  return {
+    props: {
+      microcmsDataContents
+    }
+  }
+}
 
 export default function Home() {
   return (
