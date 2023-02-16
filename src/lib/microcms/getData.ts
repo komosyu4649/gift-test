7ec926e851f4f00e9b11792458310d7ea18f8df7
komosyu4649 <komosyu4649@gmail.com>
2023-02-16 10:28:40 +0900

    add: prettierrc

--- src/lib/microcms/getData.ts
+++ src/lib/microcms/getData.ts
@@ -1,8 +1,8 @@
-import { client } from "."
+import { client } from '.'
 
-export const getMicroCMSData = async(contentId:string) => {
-    const res = await client.getList({
-        endpoint: contentId
-    })
-    return res
+export const getMicroCMSData = async (contentId: string) => {
+  const res = await client.getList({
+    endpoint: contentId,
+  })
+  return res
 }
